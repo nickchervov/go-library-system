e2708db9f814413f43fcfa28454a9c804a0b2198
Nick Chervov <nickchervoff@gmail.com>
2026-05-06 11:43:38 +0300

    added data models

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/nickchervov/go-library-system
+
+go 1.25.6
---
+++ internal/interfaces/storage.go
@@ -0,0 +1,27 @@
+package interfaces
+
+import "time"
+
+type Book struct {
+	ID        int    `json:"id"`
+	Title     string `json:"title"`
+	Author    string `json:"author"`
+	ISBN      string `json:"isbn"`
+	Year      string `json:"year"`
+	Genre     string `json:"genre"`
+	Pages     int    `json:"pages"`
+	Available bool   `json:"available"`
+}
+
+type LoanInfo struct {
+	BookID     int        `json:"book_id"`
+	MemberID   string     `json:"member_id"`
+	DueDate    time.Time  `json:"due_date"`
+	ReturnDate *time.Time `json:"return_date,omitempty"`
+}
+
+type Library struct {
+	Books   map[int]*Book
+	Members map[string][]string // [Член библиотеки][]id_книг
+	Loans   map[string]LoanInfo
+}

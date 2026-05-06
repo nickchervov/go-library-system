a4aaeb2a90541b73d01816440fe98be792a75ce6
Nick Chervov <nickchervoff@gmail.com>
2026-05-06 13:29:10 +0300

    added methods for analytics and reports

--- internal/interfaces/analytics.go
+++ internal/interfaces/analytics.go
@@ -1,8 +1,14 @@
 package interfaces
 
-import "github.com/nickchervov/go-library-system/internal/models"
+import (
+	"time"
+
+	"github.com/nickchervov/go-library-system/internal/models"
+)
 
 type AnalyticsProvider interface {
 	GetMostPopularGenre() (string, int)
+	CalculateReadTime(memberId string) time.Duration
 	FindOverdueLoans() []models.LoanInfo
+	GenerateMonthlyReport(start, end time.Time) (map[string]any, error)
 }
--- internal/models/library.go
+++ internal/models/library.go
@@ -1,7 +1,7 @@
 package models
 
 import (
-	"errors"
+	"fmt"
 	"time"
 
 	"github.com/nickchervov/go-library-system/internal/utils"
@@ -12,7 +12,7 @@ type Book struct {
 	Title     string `json:"title" validate:"required"`
 	Author    string `json:"author" validate:"required,alpha"`
 	ISBN      string `json:"isbn" validate:"required,numeric,min=13,max=13"`
-	Year      string `json:"year" validate:"required,numeric"`
+	Year      int    `json:"year" validate:"required,numeric"`
 	Genre     string `json:"genre" validate:"required,alpha"`
 	Pages     int    `json:"pages" validate:"required,numeric"`
 	Available bool   `json:"available" validate:"required,alpha"`
@@ -27,14 +27,14 @@ type LoanInfo struct {
 
 type Library struct {
 	Books   map[int]*Book
-	Members map[string][]string // [Член библиотеки][]id_книг
+	Members map[string][]int // [Член библиотеки][]id_книг
 	Loans   map[string]LoanInfo
 }
 
 func NewLibrary() *Library {
 	return &Library{
 		Books:   make(map[int]*Book),
-		Members: make(map[string][]string),
+		Members: make(map[string][]int),
 		Loans:   make(map[string]LoanInfo),
 	}
 }
@@ -46,7 +46,7 @@ var (
 
 func (l *Library) AddBook(b Book) error {
 	if err := utils.Validator.Struct(b); err != nil {
-		return errors.New("ошибка при валидации данных")
+		return fmt.Errorf("ошибка при валидации данных: %v", err)
 	}
 	if b.ID == 0 {
 		b.ID = nextId
@@ -67,4 +67,52 @@ func (l *Library) ListBook(genre string) map[int]*Book {
 		}
 	}
 	return listByGenreBooks
+}
+
+func (l *Library) GetMostPopularGenre() (string, int) {
+	genreCount := make(map[string]int)
+	for _, v := range l.Books {
+		genreCount[v.Genre]++
+	}
+
+	var mostPopularGenre string
+	var biggestCount int
+	for k, v := range genreCount {
+		if v >= biggestCount {
+			biggestCount = v
+			mostPopularGenre = k
+		}
+	}
+
+	return mostPopularGenre, biggestCount
+}
+
+func (l *Library) CalculateReadTime(memberId string) time.Duration {
+	var readTime float64
+	for _, bookIDs := range l.Members[memberId] {
+		if book, exist := l.Books[bookIDs]; exist {
+			//1 страница = 30 минут чтения
+			readTime += float64(book.Pages) * 0.5 / 24.0
+		}
+	}
+	return time.Duration(readTime * 24)
+}
+
+func (l *Library) FindOverdueLoans() []LoanInfo {
+	var overdueLoans []LoanInfo
+	for _, v := range l.Loans {
+		if v.ReturnDate != nil && v.ReturnDate.Before(time.Now()) {
+			overdueLoans = append(overdueLoans, v)
+		}
+	}
+	return overdueLoans
+}
+
+func (l *Library) GenerateMonthlyReport(start, end time.Time) (map[string]interface{}, error) {
+	report := make(map[string]interface{})
+	report["totalBooks"] = len(l.Books)
+	report["activeMembers"] = len(l.Members)
+	report["overdueLoans"] = len(l.FindOverdueLoans())
+
+	return report, nil
 }
